a7baeb5fac020f9df139783222beeb885d13301d
Christopher Wang <wang967@usc.edu>
2017-07-18 22:14:25 -0700

    complete tensorflow model

---
+++ Planet.py
@@ -0,0 +1,16 @@
+import os
+import gc
+
+import numpy as np
+import pandas as pd
+import cv2
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+
+print('asdf')
+
+
